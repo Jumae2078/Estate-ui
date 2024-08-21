2722312d4a19bf430c281da1a10392ae7fdff397
Jumae <jumae20078@gmail.com>
2024-08-20 17:21:46 -0700

    Completed the UI design

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,8 @@ import ListPage from "./routes/listPage/listPage";
 import Layout from "./routes/layout/layout";
 import SinglePage from "./routes/singlePage/SinglePage";
 import ProfilePage from "./routes/profilePage/profilePage";
+import Login from "./routes/login/login";
+import Register from "./routes/register/register";
 
 function App() {
 
@@ -31,6 +33,14 @@ function App() {
           path: "/profile",
           element: <ProfilePage />
         },
+        {
+          path:"/login",
+          element:<Login/>
+        },
+        {
+          path:"/register",
+          element:<Register/>
+        }
       ]
     },
   ]);
